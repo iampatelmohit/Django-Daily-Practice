6cbaefdb8b032b8d869ac3a6a26c9d458ac9f7b7
Mohit Patel <138792846+iampatelmohit@users.noreply.github.com>
2026-04-23 19:57:28 +0530

    Add SessionMiddleware to settings

--- todo/todoproject/todoproject/settings.py
+++ todo/todoproject/todoproject/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'todoproject.urls'
